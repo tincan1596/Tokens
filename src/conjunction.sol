a3337aa5b55284eee5b5374e2c28d60639633a2e
tincan1596 <sahilatwal2003@gmail.com>
2025-07-24 19:40:47 +0000

    CONJ token contract

--- src/conjunction.sol
+++ src/conjunction.sol
@@ -1,20 +1,67 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
- 
+
 interface IERC20 {
-    function name () external view returns (string memory);
-    function symbol () external view returns (string memory);
-    function decimals () external view returns (uint8);
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
 
-    function totalSupply () external view returns (uint256);
-    function balanceOf (address account) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
 
-    function transfer (address recipient, uint256 amount) external returns (bool);
-    function allowance (address owner, address spender) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
 
-    function approve (address spender, uint256 amount) external returns (bool);
-    function transferFrom (address sender, address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
-    event Trensfer (address indexed from, address indexed to, uint256 value);
-    event Approval (address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+abstract contract ERC20 is IERC20 {
+    string name = "Conjunction";
+    string symbol = "CONJ";
+    uint8 decimals = 18;
+
+    uint256 totalSupply;
+    mapping(address => uint256) balanceOf;
+    mapping(address => mapping(address => uint256)) allowance;
+
+    constructor(uint256 initialSupply) {
+        balanceOf[msg.sender] = totalSupply;
+        totalSupply = initialSupply;
+        emit Transfer(address(0), msg.sender, initialSupply);
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "Transfer to the zero address");
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "Approve to the zero address");
+        allowance[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 ammount) external returns (bool) {
+        require(address(0) != to, "Transfer to the zero address");
+        require(balanceOf[from] >= ammount, "Insufficient balance");
+        require(allowance[from][msg.sender] >= ammount, "Allowance exceeded");
+        balanceOf[from] -= ammount;
+        balanceOf[to] += ammount;
+        allowance[from][msg.sender] -= ammount;
+
+        emit Transfer(from, to, ammount);
+        return true;
+    }
 }
