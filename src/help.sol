1d0dee2801e40977efcdde9bab35d4659ade5e17
tincan1596 <sahilatwal2003@gmail.com>
2025-07-25 07:38:11 +0000

    HELP token using openzeppelin ERC20 and Owanble

--- src/help.sol
+++ src/help.sol
@@ -1,2 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract HELP is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("HELP", "HLP") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * 1e18); // mint initial supply of tokens
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount * 1e18); // mint additional tokens ot a specific address
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount * 1e18); // burn tokens from a specific address
+    }
+}
