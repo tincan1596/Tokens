96a4cfbf3e4bfd9cba62fef417ba44ccc055d5f7
tincan1596 <sahilatwal2003@gmail.com>
2025-07-28 08:34:35 +0000

    approval and bounding changes in handler contsct

--- test/ERC20/help_handler.t.sol
+++ test/ERC20/help_handler.t.sol
@@ -55,6 +55,31 @@ contract HelpTokenHandler {
         token.transfer(to, amount);
     }
 
+    function callApprove(uint96 amount, address spender) public {
+        if (spender == address(0) || spender == msg.sender) return;
+
+        uint256 maxAmount = token.balanceOf(msg.sender);
+        if (amount > maxAmount) {
+            amount = uint96(maxAmount);
+        }
+        // amount is now bounded between 0 and balanceOf(msg.sender)
+        vm.prank(msg.sender);
+        token.approve(spender, amount);
+    }
+
+    function callTransferFrom(uint96 amount, address from, address to) public {
+        if (from == address(0) || to == address(0) || from == to) return;
+
+        uint256 allowance = token.allowance(from, msg.sender);
+        if (allowance == 0) return;
+
+        if (amount > allowance) {
+            amount = uint96(allowance);
+        }
+        vm.prank(msg.sender);
+        token.transferFrom(from, to, amount);
+    }
+
     function getActors() external view returns (address[] memory) {
         return actors;
     }
