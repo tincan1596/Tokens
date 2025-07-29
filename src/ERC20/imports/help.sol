94f1a0137154accbbf4c5dc81c196ad15a47b4a1
tincan1596 <sahilatwal2003@gmail.com>
2025-07-29 05:35:47 +0000

    multiplier checks

--- src/ERC20/imports/help.sol
+++ src/ERC20/imports/help.sol
@@ -6,14 +6,14 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HELP is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("HELP", "HLP") Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply * 1e18); // mint initial supply of tokens
+        _mint(msg.sender, initialSupply); // mint initial supply of tokens, dont use *1e18 since decimal is by default set to 18.
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount * 1e18); // mint additional tokens ot a specific address
+        _mint(to, amount); // mint additional tokens ot a specific address
     }
 
     function burn(address from, uint256 amount) external onlyOwner {
-        _burn(from, amount * 1e18); // burn tokens from a specific address
+        _burn(from, amount); // burn tokens from a specific address
     }
 }
